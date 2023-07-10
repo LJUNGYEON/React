1e9f13e6eb34b32c0f2384e04c0fb25ae4ca5720
LJUNGYEON <1225ljk@naver.com>
2023-07-10 12:57:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post = "ReactB"
+  let [a,b] = useState(['라디오','강남','파이썬']);
+  let [goodcnt, changecnt] = useState(0);
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="black_nav">
+        <h4 id={post}>테스트 1</h4>
+      </div>
+     <button onClick={()=>{
+        let cp1 = [...a];
+        cp1.sort((a, b) => a.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
+
+        b(cp1);
+
+     }}>가나다 정렬</button>
+      <div className='list'>
+        <h4 style={{color:'red', fontSize:'16px'}}><span onClick={()=>{
+          let copy = [...a];
+          copy[0] = 'A3';
+          b(copy);
+        }}>
+          {a[0]}</span><span onClick={()=>{changecnt(goodcnt +1)}}>👍</span>{goodcnt}</h4>
+        <p>2023-07-10</p>
+      </div>
+      <div className='list'>
+        <h4 style={{color:'red', fontSize:'16px'}}>{a[1]}</h4>
+        <p>2023-07-10</p>
+      </div>
+      <div className='list'>
+        <h4 style={{color:'red', fontSize:'16px'}}>{a[2]}</h4>
+        <p>2023-07-10</p>
+      </div>
+    
     </div>
   );
 }
